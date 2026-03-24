b3ba1309745dd5b85c964367c8deb8dc5a20c007
Rajveer Singh Chandel <chandelrajveer7896@gmail.com>
2026-03-24 18:00:49 +0530

    implemented log in with google and github

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,4 +1,6 @@
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 export const NEXT_AUTH = {
   providers: [
     CredentialsProvider({
@@ -21,6 +23,14 @@ export const NEXT_AUTH = {
         };
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    GitHubProvider({
+    clientId: process.env.GITHUB_ID || "",
+    clientSecret: process.env.GITHUB_SECRET || ""
+  })
   ],
   callbacks: {
     session: ({ session, token, user }: any) => {
